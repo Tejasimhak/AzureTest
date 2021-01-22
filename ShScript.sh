4442059851bd7a38933329238ac3792d0157076c
Tejasimhak <tejasimha.k@gmail.com>
2021-01-22 18:29:14 +0530

    Add files via upload

---
+++ ShScript.sh
@@ -0,0 +1,4 @@
+pip install numpy pandas matplotlib spacy scikit seaborn nltk fasttext
+touch /home/Test/TestFile.txt 
+echo 'Some content' >> /home/Test/TestFile.txt
+date >> /home/Test/TestFile.txt
